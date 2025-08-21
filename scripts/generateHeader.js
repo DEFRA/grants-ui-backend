e1381edace9ffeaa74ab758303e05af120d47b90
Andrew Folga <andrew.folga@equalexperts.com>
2025-08-21 11:34:50 +0100

    Add a README section about s2s auth and postman and update postman collection. Add generateHeader.js script for ease of local development.

---
+++ scripts/generateHeader.js
@@ -0,0 +1,26 @@
+import 'dotenv/config'
+
+import crypto from 'crypto'
+
+const token = process.env.GRANTS_UI_BACKEND_AUTH_TOKEN
+const encryptionKey = process.env.GRANTS_UI_BACKEND_ENCRYPTION_KEY
+
+if (!token || !encryptionKey) {
+  throw new Error('Missing auth token or encryption key in environment variables')
+}
+
+const key = crypto.scryptSync(encryptionKey, 'salt', 32)
+const iv = crypto.randomBytes(16)
+const cipher = crypto.createCipheriv('aes-256-gcm', key, iv)
+
+let encrypted = cipher.update(token, 'utf8', 'base64')
+encrypted += cipher.final('base64')
+
+const authTag = cipher.getAuthTag().toString('base64')
+const ivB64 = iv.toString('base64')
+
+const encryptedToken = `${ivB64}:${authTag}:${encrypted}`
+const basicString = `:${encryptedToken}`
+const header = Buffer.from(basicString).toString('base64')
+
+console.log('Authorization: Basic ' + header)
