8507403a0a8684c63e0527838de9768e3df037d4
David Middleton <david.middleton@esynergy.co.uk>
2026-04-10 10:28:46 +0100

    TGC-1206

--- src/migrations/migrate-applicant-to-additional-answers.js
+++ src/migrations/migrate-applicant-to-additional-answers.js
@@ -1,7 +1,7 @@
 /**
- * TGC-1204: Previous to v1.208.0 `state.applicant` was stored at the top level of `state` instead of
+ * TGC-1206: Previous to v1.208.0 `state.applicant` was stored at the top level of `state` instead of
  * nested under `state.additionalAnswers.applicant`. This migration moves existing documents to
- * the new, v1.208.0+ structure. See https://eaflood.atlassian.net/browse/TGC-1204
+ * the new, v1.208.0+ structure. See https://eaflood.atlassian.net/browse/TGC-1206
  */
 export async function migrateApplicantToAdditionalAnswers(db, logger) {
   const collection = db.collection('grant-application-state')
