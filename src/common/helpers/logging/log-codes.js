7ec2580a043599a956fa985f72835c6b6cd4945c
David Capilla <david.capilla@gmail.com>
2026-09-17 16:26:54 +0100

    TGC-1520 - Update proxy setup to avoid proxy issues with global-agent

--- src/common/helpers/logging/log-codes.js
+++ src/common/helpers/logging/log-codes.js
@@ -71,11 +71,6 @@ export const LogCodes = {
       level: 'warn',
       messageFunc: (messageOptions) =>
         `Service-to-service Web Identity token rejected | reason=${messageOptions.reason}`
-    },
-    SERVICE_JWT_STRATEGY_CONFIG: {
-      level: 'info',
-      messageFunc: (messageOptions) =>
-        `Registering service-jwt strategy | jwksUri=${messageOptions.jwksUri} | issuer=${messageOptions.issuer} | audience=${messageOptions.audience}`
     }
   },
   SYSTEM: {
